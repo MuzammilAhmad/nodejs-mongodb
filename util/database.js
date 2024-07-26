c83b17169e63653aa476b83e25df23f1e2c375a0
Muzammil Ahmad <32789251+MuzammilAhmad@users.noreply.github.com>
2024-07-26 20:47:35 +0500

    mongo-db

---
+++ util/database.js
@@ -0,0 +1,28 @@
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://Muzammil:123canon@cluster0.e01xwtx.mongodb.net/shop2?retryWrites=true&w=majority&appName=Cluster0"
+  )
+    .then((client) => {
+      console.log("Connected!");
+      _db = client.db();
+      callback(client);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found!";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
